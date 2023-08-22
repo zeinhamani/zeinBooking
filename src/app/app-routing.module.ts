118252616329d2a7e15e33a3d27ece027a5fa84f
unknown <zeincheikmamy@gmail.com>
2023-08-22 12:03:59 +0200

    Adjusting main routing configs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,19 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {path:'', redirectTo:'facilities',pathMatch:'full'},
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)
+  },
+  {
+    path:'facilities',
+    loadChildren: () => import('./facilities/facilities.module').then(m => m.FacilitiesPageModule)
+  },
+  {
+    path:'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule)
+  }
 ];
 
 @NgModule({
